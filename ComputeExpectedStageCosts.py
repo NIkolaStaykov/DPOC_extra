93530d2f8c5950818fdc19d6b5c85f8bf150cd5b
mpan31415 <michael.pan31415@gmail.com>
2024-12-09 13:03:00 +0100

    feat: optimized code

--- ComputeExpectedStageCosts.py
+++ ComputeExpectedStageCosts.py
@@ -32,30 +32,25 @@ def compute_probs_and_costs(Constants):
     current_prob, swan_prob = Constants.CURRENT_PROB, Constants.SWAN_PROB
     flow_field = Constants.FLOW_FIELD
     time_cost, thruster_cost, drone_cost = Constants.TIME_COST, Constants.THRUSTER_COST, Constants.DRONE_COST
-    
 
-    # compute the indices of the respawn states
+    # Precompute the indices of the respawn states
     swan_spawn_pos = np.ones((M, N))
     swan_spawn_pos[start_pos[0], start_pos[1]] = 0
 
     start_idx_grid = start_pos[0] + start_pos[1] * M
-    respawn_idxs = np.arange(0, start_idx_grid)
-    respawn_idxs = np.append(respawn_idxs, np.arange(start_idx_grid + 1, M * N))
-    respawn_idxs = respawn_idxs*M*N + start_idx_grid
-    
-    # loop through all state indexes
-    for state_idx in range(Constants.K):
+    respawn_idxs = np.concatenate((np.arange(0, start_idx_grid), np.arange(start_idx_grid + 1, M * N))) * M * N + start_idx_grid
 
-        # loop through all admissble actions
-        for action_idx in range(Constants.L):
-            
-            # compute the expected stage cost for the current state and action
-            expected_stage_cost, next_state_probs = get_expected_stage_cost_and_transition_probs(state_idx, action_idx, respawn_idxs,
-                                                    M, N, K, goal_pos, static_drone_pos, input_space,
-                                                    current_prob, swan_prob, flow_field,
-                                                    time_cost, thruster_cost, drone_cost)
-            
-            # write the expected stage cost to the matrix
+    # Loop through all state indexes
+    for state_idx in range(K):
+        # Loop through all admissible actions
+        for action_idx in range(L):
+            # Compute the expected stage cost for the current state and action
+            expected_stage_cost, next_state_probs = get_expected_stage_cost_and_transition_probs(
+                state_idx, action_idx, respawn_idxs, M, N, K, goal_pos, static_drone_pos, input_space,
+                current_prob, swan_prob, flow_field, time_cost, thruster_cost, drone_cost
+            )
+
+            # Write the expected stage cost to the matrix
             Q[state_idx, action_idx] = expected_stage_cost
             P[state_idx, :, action_idx] = next_state_probs
 
@@ -81,12 +76,12 @@ def compute_expected_stage_cost(Constants):
         P, Q = compute_probs_and_costs(Constants)
         print("Time taken to compute P and Q: ", time.time() - start)
         values.set_value((P, Q))
-        return P
+        return Q
     else:
         Q = values.value[1]
         values.value = None
         return Q
-    
+
 ####################################################################################
 def get_expected_stage_cost_and_transition_probs(state_idx: int, action_idx: int, respawn_idxs: np.ndarray,
                             M: int, N: int, K: int, goal_coords: np.ndarray, obs_coords: np.ndarray,
@@ -102,35 +97,35 @@ def get_expected_stage_cost_and_transition_probs(state_idx: int, action_idx: int
         float: The expected stage cost
     """
     
-    # get current state coordinates for drone and swan
+    # Get current state coordinates for drone and swan
     state_coords = idx2state(state_idx)
     drone_coords = state_coords[:2].astype(int)
     swan_coords = state_coords[2:].astype(int)
     
-    # initialize the expected stage cost to zero
+    # Initialize the expected stage cost to zero
     e_cost = 0.0
     transition_probs = np.zeros(K)
     
-    # undefined states: same cell as swan, as static drone
+    # Undefined states: same cell as swan, as static drone
     if np.all(drone_coords == swan_coords) or np.all(drone_coords == obs_coords, axis=1).any():
         return e_cost, transition_probs
     
-    # if in goal (and swan not in goal), stay in goal (keep current state) with probability 1
+    # If in goal (and swan not in goal), stay in goal (keep current state) with probability 1
     if np.all(drone_coords == goal_coords):
         return e_cost, transition_probs
 
     # 4 possibilities for disturbances: {}, {drone}, {swan}, {drone, swan}
-    # for each, compute the next states, and check if new drone is needed
-    # then, add onto the (running) correponsing transition probabilities
+    # For each, compute the next states, and check if new drone is needed
+    # Then, add onto the (running) corresponding transition probabilities
     
-    # extract disturbance probabilities
+    # Extract disturbance probabilities
     current_prob = current_prob[drone_coords[0], drone_coords[1]]
     
-    # compute drone and swan disturbances
+    # Compute drone and swan disturbances
     drone_dist = flow_field[drone_coords[0], drone_coords[1]]
     swan_dist = compute_swan_movement(drone_coords, swan_coords)
     
-    # case 1: no disturbances
+    # Case 1: no disturbances
     case1_prob = (1 - current_prob) * (1 - swan_prob)
     cost, probs = get_e_cost_and_probs_by_case(drone_coords, input_space, action_idx, swan_coords, obs_coords,
                               respawn_idxs, np.array([0, 0]), np.array([0, 0]), case1_prob,
@@ -138,7 +133,7 @@ def get_expected_stage_cost_and_transition_probs(state_idx: int, action_idx: int
     e_cost += cost
     transition_probs += probs
 
-    # case 2: drone disturbance, no swan movement
+    # Case 2: drone disturbance, no swan movement
     case2_prob = (current_prob) * (1 - swan_prob)
     cost, probs = get_e_cost_and_probs_by_case(drone_coords, input_space, action_idx, swan_coords, obs_coords,
                               respawn_idxs, drone_dist, np.array([0, 0]), 
@@ -146,7 +141,7 @@ def get_expected_stage_cost_and_transition_probs(state_idx: int, action_idx: int
     e_cost += cost
     transition_probs += probs
     
-    # case 3: no drone disturbance, swan movement
+    # Case 3: no drone disturbance, swan movement
     case3_prob = (1 - current_prob) * (swan_prob)
     cost, probs = get_e_cost_and_probs_by_case(drone_coords, input_space, action_idx, swan_coords, obs_coords,
                               respawn_idxs, np.array([0, 0]), swan_dist, case3_prob,
@@ -154,7 +149,7 @@ def get_expected_stage_cost_and_transition_probs(state_idx: int, action_idx: int
     e_cost += cost
     transition_probs += probs
     
-    # case 4: drone disturbance, swan movement
+    # Case 4: drone disturbance, swan movement
     case4_prob = (current_prob) * (swan_prob)
     cost, probs = get_e_cost_and_probs_by_case(drone_coords, input_space, action_idx, swan_coords, obs_coords,
                               respawn_idxs, drone_dist, swan_dist, 
@@ -180,27 +175,27 @@ def get_e_cost_and_probs_by_case(drone_coords: np.ndarray, input_space: np.ndarr
         float: The expected stage cost.
         np.ndarray: The transition probabilities vector (to be added to the running total).
     """
-    # initilalize the transition probabilities and stage costs for all possible next states
+    # Initialize the transition probabilities and stage costs for all possible next states
     case_probs = np.zeros(K)
     case_costs = np.zeros(K)
     
-    # compute next state coordinates for drone and swan
+    # Compute next state coordinates for drone and swan
     next_drone_coords = get_drone_coords(drone_coords, input_space[action_idx], drone_dist)
     next_swan_coords = get_swan_coords(swan_coords, swan_dist)
     
-    # check if respawn is needed
+    # Check if respawn is needed
     if needs_respawn(drone_coords, next_drone_coords, next_swan_coords, obs_coords, M, N):
-        # respawn needed, add probability to all possible respawn states
+        # Respawn needed, add probability to all possible respawn states
         case_probs[respawn_idxs] = case_prob / len(respawn_idxs)
         case_costs[respawn_idxs] = compute_stage_cost(time_cost, thruster_cost, drone_cost, action_idx, 1)
     else:
-        # respawn not needed, compute next state index
+        # Respawn not needed, compute next state index
         next_state_idx = state2idx(np.concatenate((next_drone_coords, next_swan_coords)))
-        # add onto transition probability
+        # Add onto transition probability
         case_probs[next_state_idx] += case_prob
         case_costs[next_state_idx] = compute_stage_cost(time_cost, thruster_cost, drone_cost, action_idx, 0)
     
-    # compute expected stage cost
+    # Compute expected stage cost
     case_e_cost = np.dot(case_probs, case_costs)
     
     return case_e_cost, case_probs
@@ -220,7 +215,7 @@ def compute_stage_cost(time_cost, thruster_cost, drone_cost, action_idx: int, ne
     Returns:
         float: The stage cost.
     """
-    # get action and its norm
+    # Get action and its norm
     action = Constants.INPUT_SPACE[action_idx]
     action_norm = np.abs(action).sum()
     
